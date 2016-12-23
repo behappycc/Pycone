b487d977e70476c1c63dd8f0453738d7bb30eeff
behappycc <behappyccc@gmail.com>
2016-12-24 02:52:25 +0800

    add python-data-science-tutorial-2

--- website/web_server.py
+++ website/web_server.py
@@ -104,10 +104,23 @@ class SublimeAnacondaHandler(Resource):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('sublime-anaconda.html'), 200, headers)
 
+
 class PythonDataScienceTutorial1Handler(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('python-data-science-tutorial-1.html'), 200, headers)       
+        return make_response(render_template('python-data-science-tutorial-1.html'), 200, headers)
+
+
+class PythonDataScienceTutorial2Handler(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('python-data-science-tutorial-2.html'), 200, headers)
+
+
+class PythonDataScienceTutorial3Handler(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('python-data-science-tutorial-3.html'), 200, headers)
 
 
 class SearchConsoleHandler(Resource):
@@ -132,6 +145,8 @@ api.add_resource(MacPyEnvHandler, '/blogs/mac-python-environment')
 api.add_resource(LinuxPyEnvHandler, '/blogs/linux-python-environment')
 api.add_resource(SublimeAnacondaHandler, '/blogs/sublime-anaconda')
 api.add_resource(PythonDataScienceTutorial1Handler, '/blogs/python-data-science-tutorial-1')
+api.add_resource(PythonDataScienceTutorial2Handler, '/blogs/python-data-science-tutorial-2')
+api.add_resource(PythonDataScienceTutorial3Handler, '/blogs/python-data-science-tutorial-3')
 
 if __name__ == '__main__':
     main()
