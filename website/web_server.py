7fc6f4fd54b1c61c4f30a04cf025add26e41cc0d
behappycc <behappyccc@gmail.com>
2017-01-22 23:51:15 +0800

    modify blogs

--- website/web_server.py
+++ website/web_server.py
@@ -122,10 +122,12 @@ class PythonDataScienceTutorial3Handler(Resource):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('python-data-science-tutorial-3.html'), 200, headers)
 
+
 class PythonDataScienceTutorial4Handler(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('python-data-science-tutorial-4.html'), 200, headers)
+
 
 class PythonDataScienceTutorial5Handler(Resource):
     def get(self):
