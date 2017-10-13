badddf8a510796d7995a2a7fbd923157d775ce1a
hubert.lin <hubert.lin@vivotek.com>
2017-10-13 17:44:53 +0800

    Add redirect 404 page

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let express = require('express');
+const express = require('express');
 
-let app = express();
+const app = express();
 
 app.use(express.static('public'))
+
+app.use(function(req, res) {
+  res.status(400)
+  res.redirect('http://pycone.com')
+})
 
 app.listen(process.env.PORT || 8888);
